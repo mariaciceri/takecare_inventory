61aa00b15ba75ca659a5617d3a18de1d098d1b42
Maria <mariafacchin00@hotmail.com>
2025-01-26 10:59:46 +0100

    Include urls to correct htmls

--- inventory/urls.py
+++ inventory/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 
 urlpatterns = [
-    path('', login_required(include('ordering.urls')), name='ordering'),
+    path('', include('ordering.urls'), name='ordering'),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
--- ordering/urls.py
+++ ordering/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.order_view, name='order-list'),
+    path('', views.order, name='order'),
+    path('orders/', views.order_view, name='order_view'),
 ]
