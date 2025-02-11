81260b5180c79ba0a5d5eb3d54bb011bba539f1d
Maria <mariafacchin00@hotmail.com>
2025-02-11 16:29:37 +0100

    Add functions to return custom error pages

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404, handler500
+from django.shortcuts import render
 
 urlpatterns = [
     path('', include('ordering.urls'), name='ordering_urls'),
@@ -7,3 +9,12 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
+
+def custom_404(request, exception):
+    return render(request, 'errors/error-404.html', status=404)
+
+def custom_500(request):
+    return render(request, 'errors/error-500.html', status=500)
+
+handler404 = custom_404
+handler500 = custom_500
